61ca2a08fdaaabca8ab512610b0fdd269a215d81
Claire <sivadiercl@gmail.com>
2025-09-02 16:52:39 +0200

    Chg(InteractionEntity): set it as Sendable

--- Peticle/Model/InteractionEntity.swift
+++ Peticle/Model/InteractionEntity.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-final class InteractionEntity {
+final class InteractionEntity: Sendable {
     var interactionRating: InteractionRating
     
     init(interactionRating: InteractionRating) {
