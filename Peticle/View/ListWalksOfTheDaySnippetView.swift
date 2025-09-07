dae48560aa8a59fac3590e201376374ae544eabc
Claire <sivadiercl@gmail.com>
2025-09-07 12:54:51 +0200

    New(ListWalksOfTheDaySnippetView)

--- Peticle/View/ListWalksOfTheDaySnippetView.swift
+++ Peticle/View/ListWalksOfTheDaySnippetView.swift
@@ -5,4 +5,30 @@
 //  Created by Claire on 06/09/2025.
 //
 
-import Foundation
+import AppIntents
+import SwiftUI
+
+struct ListWalksOfTheDaySnippetView: View {
+    let walkEntities: [DogWalkEntryEntity]
+    
+    var body: some View {
+        if walkEntities.isEmpty {
+            noWalksView
+        } else {
+            walksList
+        }
+    }
+    
+    var walksList: some View {
+        ForEach(walkEntities) { walkEntity in
+            Button("\(walkEntity.durationInMinutes) min", intent: DeleteWalkIntent(walkEntity: walkEntity))
+        }
+    }
+    
+    var noWalksView: some View {
+        VStack() {
+            Text("WHAT?! No Walk Today!!!")
+            Image("Rotated Alfie")
+        }
+    }
+}
